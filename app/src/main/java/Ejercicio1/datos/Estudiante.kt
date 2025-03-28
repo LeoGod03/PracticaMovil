90b3c9882542c1ff5b65866722e8133ddc22c9b0
LeoGod03 <leopardo03.2001@outlook.com>
2025-03-28 11:49:15 -0600

    ejercicio 1

--- app/src/main/java/Ejercicio1/datos/Estudiante.kt
+++ app/src/main/java/Ejercicio1/datos/Estudiante.kt
@@ -1,4 +1,18 @@
 package Ejercicio1.datos
 
 interface Estudiante {
+
+    fun calcularPromedioYNota()
+    fun captutrarDatos()
+    fun getCalificacion() : Double
+    fun getSumaCalificaciones(): Double
+    fun getMatricula() : Int
+    fun getNumeroDeCalificaciones() : Int
+    fun getNombre(): String
+    fun setCalificacion(calificacion : Double)
+    fun setSumaCalificaciones(suma : Double)
+    fun setMatricula(matricula : Int)
+    fun setNumeroDeCalificaciones(numeroDeCalificaciones: Int)
+    fun setNombre(nombre: String)
+
 }
--- app/src/main/java/Ejercicio1/interfaz/Main.kt
+++ app/src/main/java/Ejercicio1/interfaz/Main.kt
@@ -1,5 +1,8 @@
 package Ejercicio1.interfaz
 
-fun main(){
+import Ejercicio1.logica.EvaluandoEstudiante
 
+fun main(){
+    val evalEstudiante = EvaluandoEstudiante()
+    evalEstudiante.calcularPromedioYNota()
 }
