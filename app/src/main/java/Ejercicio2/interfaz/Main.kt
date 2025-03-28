359a7c6967ac2f81c5bf4dee2c25101c0f6efb34
FabrexG <brian.escalona@alumnos.uacm.edu.mx>
2025-03-28 12:38:31 -0600

    terminar funciones
    Se implementaron las funciones faltantes

--- app/src/main/java/Ejercicio2/interfaz/Main.kt
+++ app/src/main/java/Ejercicio2/interfaz/Main.kt
@@ -3,8 +3,6 @@ package Ejercicio2.interfaz
 import Ejercicio2.logica.VolumenCilindro
 
 fun main(){
-    println("Ingrese la altura: ")
-    val altura = readlnOrNull()?.toDoubleOrNull()?:1.0
-    println("Ingresa el radio: ")
-    val radio= readlnOrNull()?.toDoubleOrNull()?:1.0
+    val volumenCilindro = VolumenCilindro()
+    volumenCilindro.capturarDatos()
 }
--- app/src/main/java/Ejercicio2/logica/VolumenCilindro.kt
+++ app/src/main/java/Ejercicio2/logica/VolumenCilindro.kt
@@ -2,6 +2,8 @@ package Ejercicio2.logica
 
 import Ejercicio2.datos.Pi
 import Ejercicio2.datos.Dimensiones
+import kotlin.math.pow
+
 class VolumenCilindro: Pi, Dimensiones{
     //variables a ocupar
     private var altura: Double = 0.0
@@ -9,18 +11,60 @@ class VolumenCilindro: Pi, Dimensiones{
     private var numeroDeELemento: Int= 0
 
     //Pi
-    override fun getNumeroDeElementos(): Int {return this.numeroDeELemento}
-    override fun setNumeroDeElementos(numeroDeELemento: Int) {require(numeroDeELemento>0){"El numero de elemento debe ser mayor a cero"}}
-    override fun calcularPI(): Double {return Math.PI}
+    override fun getNumeroDeElementos(): Int {
+        return this.numeroDeELemento
+    }
+    override fun setNumeroDeElementos(numeroDeELemento: Int) {
+        require(numeroDeELemento>0){"El numero de elemento debe ser mayor a cero"}
+        this.numeroDeELemento = numeroDeELemento
+    }
+    override fun calcularPI(): Double {
+        var puntosDentroCirculo = 0.0
+        for (i in 0..getNumeroDeElementos()){
+            val x = Math.random()
+            val y = Math.random()
+            if (x.pow(2.0) + y.pow(2.0) <= 1){
+                puntosDentroCirculo++
+            }
+        }
+        return 4 * puntosDentroCirculo / getNumeroDeElementos()
+    }
     //Dimensiones
-    override fun getAltura(): Double {return this.altura}
-    override fun getRadio(): Double {return this.radio}
-    override fun setAltura(altura: Double) {require(altura>0){"La altura debe ser mayor a cero"}}
-    override fun setRadio(radio: Double) {require(radio>0){"El radio debe ser mayo a cero"}}
+    override fun getAltura(): Double {
+        return this.altura
+    }
+    override fun getRadio(): Double {
+        return this.radio
+    }
+    override fun setAltura(altura: Double) {
+        require(altura>0){"La altura debe ser mayor a cero"}
+        this.altura = altura
+    }
+    override fun setRadio(radio: Double) {
+        require(radio>0){"El radio debe ser mayo a cero"}
+        this.radio = radio
+    }
 
-    fun calcularVolumenCilindro(){
+    private fun calcularVolumenCilindro(){
         val pi = calcularPI()
-        val volumenCilindro = pi * Math.pow(radio,2.0) * altura
-        println("El volumen del cilindro es: $volumenCilindro")
+        println("El valor de PI es: $pi")
+        val volumenCilindro = pi * radio.pow(2.0) * altura
+        println("El volumen del cilindro es %.3f".format(volumenCilindro))
+    }
+
+
+    fun capturarDatos() {
+
+        println("Ingrese el número de elementos para calular PI: ")
+        setNumeroDeElementos(readlnOrNull()?.toIntOrNull()?:1)
+
+        println("Ingresa el radio: ")
+        setRadio(readlnOrNull()?.toDoubleOrNull()?:1.0)
+
+        println("Ingrese la altura: ")
+        setAltura(readlnOrNull()?.toDoubleOrNull()?:1.0)
+
+        val volumen = calcularVolumenCilindro()
+
     }
 }
