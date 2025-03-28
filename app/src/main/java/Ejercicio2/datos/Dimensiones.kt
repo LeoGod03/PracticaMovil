0640f4ee9574648770201495595fa7f9b54b65ad
Juan Granados <garj980511@gmail.com>
2025-03-28 11:50:26 -0600

    Versión de Juan
    Creación e implementación de las clases e interfaces, falta crear el Main

--- app/src/main/java/Ejercicio2/datos/Dimensiones.kt
+++ app/src/main/java/Ejercicio2/datos/Dimensiones.kt
@@ -1,4 +1,9 @@
 package Ejercicio2.datos
 
 interface Dimensiones {
+    fun getAltura():Double
+    fun getRadio():Double
+    fun setAltura(altura: Double)
+    fun setRadio(radio:Double)
+
 }
--- app/src/main/java/Ejercicio2/logica/VolumenCilindro.kt
+++ app/src/main/java/Ejercicio2/logica/VolumenCilindro.kt
@@ -1,4 +1,26 @@
 package Ejercicio2.logica
 
-class VolumenCilindro {
+import Ejercicio2.datos.Pi
+import Ejercicio2.datos.Dimensiones
+class VolumenCilindro: Pi, Dimensiones{
+    //variables a ocupar
+    private var altura: Double = 0.0
+    private var radio: Double = 0.0
+    private var numeroDeELemento: Int= 0
+
+    //Pi
+    override fun getNumeroDeElementos(): Int {return this.numeroDeELemento}
+    override fun setNumeroDeElementos(numeroDeELemento: Int) {require(numeroDeELemento>0){"El numero de elemento debe ser mayor a cero"}}
+    override fun calcularPI(): Double {return Math.PI}
+    //Dimensiones
+    override fun getAltura(): Double {return this.altura}
+    override fun getRadio(): Double {return this.radio}
+    override fun setAltura(altura: Double) {require(altura>0){"La altura debe ser mayor a cero"}}
+    override fun setRadio(radio: Double) {require(radio>0){"El radio debe ser mayo a cero"}}
+
+    fun calcularVolumenCilindro(){
+        val pi = calcularPI()
+        val volumenCilindro = pi * Math.pow(radio,2.0) * altura
+        println("El volumen del cilindro es: $volumenCilindro")
+    }
 }
